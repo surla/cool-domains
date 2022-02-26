e27879320705382cf1a45833aa8ff28c3fb22e7f
Joseph Surla <josephsurla@gmail.com>
2022-02-26 11:18:32 -0800

    store domain data on-chain

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,39 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+	// A "mapping data type to store their names
+	mapping(string => address) public domains;
+
+	// This will store values
+	mapping(string => string) public records;
+
 	constructor() {
-		console.log("THIS IS MY DOMAINS CONTACT. NICE.");
+		console.log("Yo yo, I am a contract and I am smart");
 	}
+
+	// A register function that adds their names to our mapping
+	function register(string calldata name) public {
+		// Check that the name is unregistered
+		require(domains[name] == address(0));
+		domains[name] = msg.sender;
+		console.log("%s has registered a domain!", msg.sender);
+	}
+
+	// This will give us the domain owners' address
+	function getAddress(string calldata name) public view returns (address) {
+		// Check that the owner is the transaction sender
+		return domains[name];
+	}
+
+	function setRecord(string calldata name, string calldata record) public {
+		// Check that the owner is the transaction sender
+		require(domains[name] == msg.sender);
+		records[name] = record;
+	}
+
+	function getRecord(string calldata name) public view returns(string memory) {
+		return records[name];
+	}
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,16 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
-  console.log("Contract deploy to:", domainContract.address);
+  console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  const txn = await domainContract.register("doom");
+  await txn.wait();
+
+  const domainAddress = await domainContract.getAddress("doom");
+  console.log("Owner ot domain doom:", domainAddress);
 };
 
 const runMain = async () => {
