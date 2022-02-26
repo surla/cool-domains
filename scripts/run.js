ee5afc0cbec026471ab845468d209370bcb88355
Joseph Surla <josephsurla@gmail.com>
2022-02-26 11:48:02 -0800

    add nft minting

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,22 @@
+const { ethers } = require("ethers");
+
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy("ninja");
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
 
-  const txn = await domainContract.register("doom");
+  // Passing in a second variable - value. This is the money.
+  const txn = await domainContract.register("mortal", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await txn.wait();
 
-  const domainAddress = await domainContract.getAddress("doom");
+  const address = await domainContract.getAddress("mortal");
   console.log("Owner ot domain doom:", domainAddress);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
